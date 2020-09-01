0bb639e6d3bb57d1e065b635052d601b74ee9947
MuthuSubbuLN <MuthuSubramanian_LakshmiNarayanan@flightcentre.com>
2020-09-01 14:59:22 +0100

    few corrections

--- main.py
+++ main.py
@@ -3,15 +3,6 @@
 import pandas as pd
 import glob as glob
 
-# import csv 
-# Create CSV files
-# with open('innovators.csv', 'w', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["SN", "Name", "Contribution"])
-#     writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
-#     writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
-#     writer.writerow([3, "Guido van Rossum", "Python Programming"])
-
 header_names = ['SN1', 'Name1', 'Cont1']
 
 path = r'csvfiles/' # use your path
@@ -19,8 +10,10 @@ all_files = glob.glob(path + "*.csv")
 new_path = glob.glob(r'csvfiles1/')
 
 for filename in all_files:
-    print('\n source path : {filename}')
-    print('destination path : csvfiles1/{filename.split('/')[1]}')
+    print('\n source path : ' , filename)
+    # pp = r'csvfiles1/{filename.split('/')[-1]}'
+    pp = "{}/{}".format("csvfiles1", filename[1])
+    print('destination path : ' , pp)
     df = pd.read_csv(filename, header=None,skiprows=1,names=header_names)
     df.to_csv(pp, index = False, header=True)
 
@@ -28,7 +21,7 @@ print('£££ modified files exported to new location! \n')
 
 print('==> read files from destination path <== \n')
 
-files = glob.glob(r'csvfiles1/*.csv")
+files = glob.glob(r"csvfiles1/*.csv")
 
 for destFile in files:
     print('\n log1', pd.read_csv(destFile, sep=";"))
