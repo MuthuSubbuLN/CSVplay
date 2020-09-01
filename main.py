347db59741423c474afd26687e81d945be2aa9c4
MuthuSubbuLN <MuthuSubramanian_LakshmiNarayanan@flightcentre.com>
2020-09-01 15:15:43 +0100

    few more corrections

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import glob as glob
 
 header_names = ['SN1', 'Name1', 'Cont1']
 
-path = r'csvfiles/' # use your path
+path = r'csvfiles/' # use your path - this is source folder
 all_files = glob.glob(path + "*.csv")
-new_path = glob.glob(r'csvfiles1/')
+new_path = glob.glob(r'csvfiles1/') # use your path - this is destination folder
 
 for filename in all_files:
     print('\n source path : ' , filename)
-    # pp = r'csvfiles1/{filename.split('/')[-1]}'
-    pp = "{}/{}".format("csvfiles1", filename[1])
+    file = filename.split('/')
+    print(file[-1])
+    pp = "{}/{}".format("csvfiles1", file[-1]) # making destination path
     print('destination path : ' , pp)
     df = pd.read_csv(filename, header=None,skiprows=1,names=header_names)
     df.to_csv(pp, index = False, header=True)
